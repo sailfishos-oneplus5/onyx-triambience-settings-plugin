8e12ad18e0ecc01186849160560af51fdd369595
Kimmo Lindholm <kimmo.lindholm@eke.fi>
2016-04-09 14:03:57 +0300

    add translations; de, pl

---
+++ settings/i18n/onyx-triambience-settings_de.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="de" version="2.1">
+<context>
+    <name/>
+    <message id="onyx-tristate-settings-title">
+        <source>Tristate ambience selector</source>
+        <extracomment>page header</extracomment>
+        <translation>Tristate Ambiente Wechsler</translation>
+    </message>
+    <message id="onyx-top-position">
+        <source>Top</source>
+        <translation>Oben</translation>
+    </message>
+    <message id="onyx-middle-position">
+        <source>Middle</source>
+        <translation>Mitte</translation>
+    </message>
+    <message id="onyx-bottom-position">
+        <source>Bottom</source>
+        <translation>Unten</translation>
+    </message>
+</context>
+</TS>
--- settings/i18n/onyx-triambience-settings_pl.ts
+++ settings/i18n/onyx-triambience-settings_pl.ts
@@ -1,24 +1,22 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="pl" version="2.1">
 <context>
-    <name></name>
+    <name/>
     <message id="onyx-tristate-settings-title">
         <source>Tristate ambience selector</source>
         <extracomment>page header</extracomment>
-        <translation>Kolmitilakytkimen tunnelmat</translation>
+        <translation>Wybieranie atmosfery</translation>
     </message>
     <message id="onyx-top-position">
         <source>Top</source>
-        <translation>Yläasento</translation>
+        <translation>Góra</translation>
     </message>
     <message id="onyx-middle-position">
         <source>Middle</source>
-        <translation>Keskiasento</translation>
+        <translation>Środek</translation>
     </message>
     <message id="onyx-bottom-position">
         <source>Bottom</source>
-        <translation>Ala-asento</translation>
+        <translation>Dół</translation>
     </message>
 </context>
 </TS>
